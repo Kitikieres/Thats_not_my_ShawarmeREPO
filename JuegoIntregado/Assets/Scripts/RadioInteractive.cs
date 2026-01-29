a0490e115571e55a88e6a2f0d1ae4be66f71afb6
Kitikieres <palmerjuan1670@gmail.com>
2026-01-29 12:56:40 +0100

    alibaba

---
+++ JuegoIntregado/Assets/Scripts/RadioInteractive.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class RadioInteractiva : MonoBehaviour
+{
+    [Header("Audio")]
+    public AudioSource musicaRadio;
+
+    private bool encendida = false;
+
+    private void Start()
+    {
+        if (musicaRadio == null)
+        {
+            musicaRadio = GetComponent<AudioSource>();
+        }
+
+        musicaRadio.loop = true;
+        musicaRadio.Stop();
+        encendida = false;
+    }
+
+    private void OnMouseDown()
+    {
+        if (encendida)
+        {
+            ApagarRadio();
+        }
+        else
+        {
+            EncenderRadio();
+        }
+    }
+
+    void EncenderRadio()
+    {
+        encendida = true;
+        musicaRadio.Play();
+        Debug.Log("📻 Radio encendida");
+    }
+
+    void ApagarRadio()
+    {
+        encendida = false;
+        musicaRadio.Stop();
+        Debug.Log("📻 Radio apagada");
+    }
+}
