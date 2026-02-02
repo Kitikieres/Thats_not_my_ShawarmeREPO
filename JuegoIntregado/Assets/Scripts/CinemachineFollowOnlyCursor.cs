86d9d621e6b5a4e668c1ed2e5cf2708f0263fff2
Kitikieres <palmerjuan1670@gmail.com>
2026-02-02 10:41:17 +0100

    aaaaa

--- JuegoIntregado/Assets/Scripts/CinemachineFollowOnlyCursor.cs
+++ JuegoIntregado/Assets/Scripts/CinemachineFollowOnlyCursor.cs
@@ -1,12 +1,11 @@
-using UnityEngine;
-using Cinemachine;
+ï»¿using UnityEngine;
+using Unity.Cinemachine;
+using UnityEngine.InputSystem;
 
 public class CinemachineFollowOnlyCursor : MonoBehaviour
 {
-    [Header("Referencias")]
-    public CinemachineVirtualCamera virtualCamera;
+    public CinemachineCamera virtualCamera;
 
-    [Header("Configuración")]
     [Range(0f, 1f)]
     public float smoothSpeed = 0.15f;
 
@@ -17,23 +16,24 @@ public class CinemachineFollowOnlyCursor : MonoBehaviour
         mainCamera = Camera.main;
 
         if (virtualCamera == null)
-            virtualCamera = GetComponent<CinemachineVirtualCamera>();
+            virtualCamera = GetComponent<CinemachineCamera>();
 
-        // Creamos un objeto vacío para que la cámara lo siga
         GameObject followTarget = new GameObject("CursorFollowTarget");
         virtualCamera.Follow = followTarget.transform;
     }
 
     void Update()
     {
-        Vector3 mouseScreenPos = Input.mousePosition;
-        mouseScreenPos.z = Mathf.Abs(mainCamera.transform.position.z);
+        
+        Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
 
-        Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(mouseScreenPos);
+        Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(
+            new Vector3(mouseScreenPos.x, mouseScreenPos.y,
+            Mathf.Abs(mainCamera.transform.position.z))
+        );
 
         Transform follow = virtualCamera.Follow;
 
-        // Movimiento suave hacia el cursor
         follow.position = Vector3.Lerp(
             follow.position,
             new Vector3(mouseWorldPos.x, mouseWorldPos.y, follow.position.z),
